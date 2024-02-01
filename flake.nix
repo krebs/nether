d810be136baf23eb16a6d511baa8a3e51a98b4dd
lassulus <git@lassul.us>
2024-02-01 15:30:33 +0100

    zerotier fixes

--- flake.nix
+++ flake.nix
@@ -33,12 +33,12 @@
           nixpkgs.lib.nameValuePair name ((nixpkgs.lib.importJSON ./hosts/${file}) // { inherit name; })
       ) (builtins.readDir ./hosts);
       network-id = builtins.readFile ./machines/controller/facts/zerotier-network-id;
-      ipForHost = nwid: id: builtins.concatStringsSep ":" ((p: builtins.substring (p * 4) 4 "${nwid}9993${id}") 8);
+      ipForHost = nwid: id: builtins.concatStringsSep ":" (builtins.genList (p: builtins.substring (p * 4) 4 "${nwid}9993${id}") 8);
     };
     nixosModules = {
       hosts = {
         networking.extraHosts = nixpkgs.lib.concatMapStringsSep "\n" (host:
-          "${self.lib.ipForHost self.lib.network-id host.addr} ${host.name}.n"
+          "${self.lib.ipForHost self.lib.network-id host.address} ${host.name}.n"
         ) (nixpkgs.lib.attrValues self.lib.hosts);
       };
       zerotier = {
@@ -46,12 +46,12 @@
       };
       controller = { pkgs, ... }: {
         systemd.services.nether-autoaccept = {
-          wanted-by = [ "multi-user.target" ];
+          wantedBy = [ "multi-user.target" ];
           after = [ "zerotierone.service" ];
           path = [ pkgs.zerotier ];
-          serviceConfig.ExecStart = pkgs.writeScript "nether-autoaccept" nixpkgs.lib.concatMapStringsSep "\n" (host: ''
+          serviceConfig.ExecStart = pkgs.writeScript "nether-autoaccept" (nixpkgs.lib.concatMapStringsSep "\n" (host: ''
             ${clan-core.packages.${pkgs.system}.zerotier-members}/bin/zerotier-members allow ${host.address}
-          '') (nixpkgs.lib.attrValues self.lib.hosts);
+          '') (nixpkgs.lib.attrValues self.lib.hosts));
         };
       };
     };
