dda25319f9d60c6e7594d92380e0f5631fec94fd
lassulus <git@lassul.us>
2024-02-05 03:09:19 +0100

    controller: add & propagate dns

--- flake.nix
+++ flake.nix
@@ -11,7 +11,9 @@
     inherit (clan-core.lib.buildClan {
       clanName = "nether";
       directory = self;
-      machines.controller = {
+      machines.controller = let
+        ip = self.lib.ipForHost self.lib.network-id self.lib.hosts.controller.address;
+      in {
         imports = [
           self.nixosModules.controller
           self.nixosModules.hosts
@@ -22,8 +24,26 @@
             enable = true;
             public = false;
           };
+          settings.dns = {
+            domain = "n";
+            servers = [
+              "${ip}/88"
+            ];
+          };
         };
-        clan.networking.deploymentAddress = "root@157.90.232.92";
+        services.dnsmasq = {
+          enable = true;
+          resolveLocalQueries = false;
+          # no idea how to do this with settings
+          extraConfig = ''
+            bind-interfaces
+          '';
+          settings = {
+            interface = "zttzibeakb"; # TODO get this interface from network id
+            listen-address = ip;
+          };
+        };
+        networking.firewall.interfaces."zt+".allowedUDPPorts = [ 53 ];
         services.openssh.enable = true;
         users.users.root.openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDIb3uuMqE/xSJ7WL/XpJ6QOj4aSmh0Ga+GtmJl3CDvljGuIeGCKh7YAoqZAi051k5j6ZWowDrcWYHIOU+h0eZCesgCf+CvunlXeUz6XShVMjyZo87f2JPs2Hpb+u/ieLx4wGQvo/Zw89pOly/vqpaX9ZwyIR+U81IAVrHIhqmrTitp+2FwggtaY4FtD6WIyf1hPtrrDecX8iDhnHHuGhATr8etMLwdwQ2kIBx5BBgCoiuW7wXnLUBBVYeO3II957XP/yU82c+DjSVJtejODmRAM/3rk+B7pdF5ShRVVFyB6JJR+Qd1g8iSH+2QXLUy3NM2LN5u5p2oTjUOzoEPWZo7lykZzmIWd/5hjTW9YiHC+A8xsCxQqs87D9HK9hLA6udZ6CGkq4hG/6wFwNjSMnv30IcHZzx6IBihNGbrisrJhLxEiKWpMKYgeemhIirefXA6UxVfiwHg3gJ8BlEBsj0tl/HVARifR2y336YINEn8AsHGhwrPTBFOnBTmfA/VnP1NlWHzXCfVimP6YVvdoGCCnAwvFuJ+ZuxmZ3UzBb2TenZZOzwzV0sUzZk0D1CaSBFJUU3oZNOkDIM6z5lIZgzsyKwb38S8Vs3HYE+Dqpkfsl4yeU5ldc6DwrlVwuSIa4vVus4eWD3gDGFrx98yaqOx17pc4CC9KXk/2TjtJY5xmQ== lass@yubikey"
